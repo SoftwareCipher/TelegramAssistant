766e9a64b91e8133ea2a9e87fc95d889223e6c6f
observerUA <kirillkuznetsov181@gmail.com>
2024-08-14 21:22:22 +0300

    start project Telegram Bot

---
+++ src/main/java/com/telegrambot/TelegramBotApplication.java
@@ -0,0 +1,18 @@
+package com.telegrambot;
+
+import com.telegrambot.config.ConfigBot;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@SpringBootApplication
+public class TelegramBotApplication {
+
+    public static void main(String[] args) throws TelegramApiException {
+        SpringApplication.run(TelegramBotApplication.class, args);
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        botsApi.registerBot(new ConfigBot());
+    }
+}
---
+++ src/main/java/com/telegrambot/config/ConfigBot.java
@@ -0,0 +1,37 @@
+package com.telegrambot.config;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class ConfigBot extends TelegramLongPollingBot {
+    @Override
+    public String getBotUsername() {
+        return "VirtualTelegramAssistant";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "7039458777:AAEQw_DRLhgusX79kObb5FeD52cws6X7d4U";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        var msg = update.getMessage();
+        var user = msg.getFrom();
+
+        System.out.println(user.getFirstName() + " wrote " + msg.getText());
+    }
+
+    public void sendText(Long who, String what){
+        SendMessage sm = SendMessage.builder()
+                .chatId(who.toString()) //Who are we sending a message to
+                .text(what).build();    //Message content
+        try {
+            execute(sm);                        //Actually sending the message
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);      //Any error will be printed here
+        }
+    }
+}
